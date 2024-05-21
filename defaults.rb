2287a0ac09850db64406fb83e081485a087a08ae
martin059 <41234752+martin059@users.noreply.github.com>
2024-05-21 23:52:41 +0200

    Fixed default role setting

--- defaults.rb
+++ defaults.rb
@@ -5,5 +5,18 @@ require 'yaml'
 
 $box = "almalinux/9"
 
+begin
+  custom = YAML::load_file(File.join(__dir__, 'ansible/group_vars/all/custom'))
+  $install_docker = custom['install_docker'] == true
+  $install_nodejs = custom['install_nodejs'] == true
+  $install_vscode = custom['install_vscode'] == true
+  $install_postman = custom['install_postman'] == true
+  $install_pgadmin = custom['install_pgadmin'] == true
+  $install_ghcli = custom['install_ghcli'] == true
+rescue
+  $install_docker = true
+  $install_nodejs = true
+end
+
 $defaultCPUs = 4
 $defaultMemory = 4 * 1024
