db2cc913a4a47762afabad774bd7f0572ac200dc
L. Martin <41234752+martin059@users.noreply.github.com>
2024-05-30 20:14:50 +0200

    #55 add default grafana base config (#58)
    * Add premade grafana config without Slack webhook
    
    * Enhanced script to take into account new use cases
    
    * Add option to choose whether Grafana is preconfigured or not
    
    * Update README

--- ansible/roles/grafana/files/grafanaUtils/restoreConfig.sh
+++ ansible/roles/grafana/files/grafanaUtils/restoreConfig.sh
@@ -1,15 +1,15 @@
 #!/bin/bash
 
 # Define the default backup directory
-BACKUP_DIR="/home/vagrant/"
+WORK_DIR="/home/vagrant/"
 
 # Check if a backup directory parameter is provided and is a valid path
 if [ $# -ge 1 ] && [ -d "$1" ]; then
-    BACKUP_DIR="$1"
+    WORK_DIR="$1"
 fi
 
-# Check if a password parameter is provided
-if [ $# -ge 2 ]; then
+# Check if a password parameter is provided and is not null or empty
+if [ $# -ge 2 ] && [ -n "$2" ]; then
     PASSWORD="$2"
 fi
 
@@ -18,6 +18,7 @@ CONTAINER_NAME="grafana"
 DATA_PATH="/var/lib/grafana/"
 DATA_FILE="grafana.db"
 ENCRYPTED_FILE="grafana.db.zip"
+OPEN_FILE="grafanaWithoutPasswd.db.zip"
 
 # Check if Docker is installed
 if ! command -v docker &> /dev/null; then
@@ -32,27 +33,56 @@ if ! docker ps -a --format '{{.Names}}' | ack "^grafana$"; then
 fi
 
 # Check if the backup file exists
-if [ ! -f "$BACKUP_DIR$ENCRYPTED_FILE" ]; then
-    if [ ! -f "$BACKUP_DIR$DATA_FILE" ]; then
-        echo "No backup file '$BACKUP_DIR$DATA_FILE' or $BACKUP_DIR$ENCRYPTED_FILE was found. Please make sure the file is available before running this script."
-        exit 1
+if [ ! -f "$WORK_DIR$DATA_FILE" ]; then
+    BU_UNZIPPED=false # BackUp file is zipped
+    if [ ! -f "$WORK_DIR$ENCRYPTED_FILE" ]; then
+        if [ ! -f "$WORK_DIR$OPEN_FILE" ]; then
+            echo "No backup file '$WORK_DIR$DATA_FILE', '$WORK_DIR$ENCRYPTED_FILE' or   '$WORK_DIR$OPEN_FILE' was found. Please make sure at least one of those files is  available before running this script."
+            exit 1
+        else
+            BU_ENCRYPTED=false
+        fi
     else
-        BU_ENCRYPTED=false
+        BU_ENCRYPTED=true
     fi
 else
-    BU_ENCRYPTED=true
+    BU_UNZIPPED=true
 fi
 
-# Unzip the file if it is encrypted
-if [ "$BU_ENCRYPTED" = true ]; then
-    unzip -P "$PASSWORD" "$BACKUP_DIR$ENCRYPTED_FILE" -d "$BACKUP_DIR"
+# Check if back up configuration needs to be unzipped, if so unzip it
+if [ "$BU_UNZIPPED" = false ]; then
+    echo "Backup file will be unziped."
+    # Unzip the file if it is encrypted
+    if [ "$BU_ENCRYPTED" = true ] && [ -n "$PASSWORD" ]; then
+        unzip -P "$PASSWORD" "$WORK_DIR$ENCRYPTED_FILE" -d "$WORK_DIR"
+        if [ $? -ne 0 ]; then
+            ENCRYPTED_FILE_OPENED=false
+            echo "Deciphering the backup file failed."
+        else
+            echo "Encrypted backup file unzipped."
+            ENCRYPTED_FILE_OPENED=true
+        fi
+    else
+        ENCRYPTED_FILE_OPENED=false
+    fi
+
+    # If the file wasn't encrypted or the password was incorrect, unzip the open file
+    if [ "$ENCRYPTED_FILE_OPENED" = false ]; then
+        echo "Trying to open the unencrypted backup file..."
+        unzip "$WORK_DIR$OPEN_FILE" -d "$WORK_DIR"
+    fi
+else
+    echo "Backup file found already unzipped."
 fi
 
 # Load backup the Grafana data
-docker cp "$BACKUP_DIR$DATA_FILE" "$CONTAINER_NAME:$DATA_PATH"
+docker cp "$WORK_DIR$DATA_FILE" "$CONTAINER_NAME:$DATA_PATH"
 
 # Fix ownership
 docker exec -u root $CONTAINER_NAME chown grafana:root $DATA_PATH$DATA_FILE
+
+# Fix execution permissions
+docker exec -u root $CONTAINER_NAME chmod +rw $DATA_PATH$DATA_FILE
 
 echo "Restarting Grafana..."
 
